7ef142c83ceef9f3f777f417425a3d6cabaf5650
Paras Shah <parasshah195@gmail.com>
2024-05-23 18:03:39 +0530

    Add print page functionality

--- src/global.ts
+++ src/global.ts
@@ -1,7 +1,9 @@
 import { initFontSizeController } from './components/font-size-controls';
 import { scrollHandler as navScrollHandler } from './components/nav-scroll';
+import { printPage } from './components/print-page';
 
 window.Webflow?.push(() => {
   navScrollHandler();
   initFontSizeController();
+  printPage();
 });
