30ec3c29c2033f496e13195a92aa329d64f6983b
Paras Shah <parasshah195@gmail.com>
2024-06-03 10:29:18 +0530

    Add GSAP globally

--- src/global.ts
+++ src/global.ts
@@ -1,8 +1,14 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import { initFontSizeController } from './components/font-size-controls';
 import { scrollHandler as navScrollHandler } from './components/nav-scroll';
 import { printPage } from './components/print-page';
 import { showRichTextSoloLinksAsButtons } from './components/rich-text-link-buttons';
 
+window.gsap = gsap;
+window.gsap.registerPlugin(ScrollTrigger);
+window.ScrollTrigger = ScrollTrigger;
 window.Webflow?.push(() => {
   navScrollHandler();
   initFontSizeController();
